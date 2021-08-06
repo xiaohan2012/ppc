a2b6440d11f552550ae4d6645341e47b25e903a7
Han Xiao <han.xiao@aalto.fi>
2021-08-06 09:51:37 +0300

    stackoverflow bug fix

--- cp2b/cp.cc
+++ cp2b/cp.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include "stdlib.h"
 using namespace std;
 
 /*
@@ -11,7 +12,7 @@ This is the function you need to implement. Quick reference:
 - only parts with 0 <= j <= i < ny need to be filled
 */
 void correlate(int ny, int nx, const float *data_, float *result) {
-  double data[nx*ny];
+  double* data = (double*) malloc(nx*ny * sizeof(double));
 
   for(int y=0; y<ny; y++){
     double sum = 0, sum_sq = 0, mean, sum_sq_sqrt;
@@ -44,5 +45,7 @@ void correlate(int ny, int nx, const float *data_, float *result) {
       result[i + j*ny] = res;
       asm("#foo end");
     }
-  }  
+  }
+
+  free(data);
 }
